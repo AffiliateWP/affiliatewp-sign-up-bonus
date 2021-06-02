3cac39ce28fc0b05a037761dbae530724d32f1ef
dbeja <david.beja@gmail.com>
2021-06-02 16:51:44 +0100

    Recreate main plugin file. #17

---
+++ affiliatewp-sign-up-bonus.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Sign Up Bonus
+ * Plugin URI: http://affiliatewp.com/addons/sign-up-bonus/
+ * Description: Entice more affiliates to register by offering them a sign up bonus
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.1
+ * Text Domain: affiliatewp-sign-up-bonus
+ * Domain Path: languages
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ */
